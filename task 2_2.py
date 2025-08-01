c66d09ff65fe4f45851a1c3d97be34cd5b108fd9
grathan-P <grathanpbangera@gmail.com>
2025-08-01 21:03:25 +0400

    task 2 added

---
+++ task 2_2.py
@@ -0,0 +1,4 @@
+FirstName=input("Enter your first name: ")
+LastName=input("Enter your last name: ")
+FullName=FirstName + " " + LastName
+print("Hello, ",FullName,"! Welcome to the python program",)
