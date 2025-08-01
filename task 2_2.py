0c2ac5441dcf12c6b14dba7613399a88664a3357
grathan-P <grathanpbangera@gmail.com>
2025-08-01 21:24:54 +0400

    Delete task 2_2.py

--- task 2_2.py
+++
@@ -1,4 +0,0 @@
-FirstName=input("Enter your first name: ")
-LastName=input("Enter your last name: ")
-FullName=FirstName + " " + LastName
-print("Hello, ",FullName,"! Welcome to the python program",)
